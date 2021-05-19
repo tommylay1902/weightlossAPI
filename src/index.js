45219132c73ed269098d460fad4a6c268f97fa00
tommylay1902 <tommylay.c@gmail.com>
2021-05-19 12:24:32 -0700

    setup some security middleware 1. add rate limiter 2. use of helmet http header library

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 const express = require("express");
-const app = express();
-const db = require("./models");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+
 const userRouter = require("./routes/userRouter");
 const macroRouter = require("./routes/macroRouter");
+const db = require("./models");
 
+const app = express();
+
+//set up helmet
+app.use(helmet());
+
+//set up rate limit
+const limiter = rateLimit({
+    windowMS: 15 * 60 * 1000, //15 minutes
+    max: 100, //only 100 request per IP every ^^^ minutes
+    delayMs: 0, // delay time between request
+});
+
+app.use(limiter);
+app.use(cors());
 app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/macros", macroRouter);
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync().then(() => {
     app.listen(3000, () => console.log("running"));
 });
